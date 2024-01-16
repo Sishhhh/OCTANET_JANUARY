a1d7151dfc9d7358c85f14132dcfee02567578c7
Snehasish Jena <snehasish.happy@gmail.com>
2024-01-16 19:38:09 +0530

    done and dusted

---
+++ Task 2/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <main>
+       <div>
+          <Todo/>
+       </div>
+
+    </main>
+  );
+}
+
+export default App;
